cfb8c56ff1cce8598d44d51c485c4cf6a3a2c085
Rohan SIngh <rs7289579@gmail.com>
2022-12-29 19:09:48 +0530

    initial commit

---
+++ client/src/components/blog/Blog.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <section>
+        <div className="h-64 bg-gray-900 text-gray-100 md:px-32 flex items-center">
+            <p className='text-4xl font-extrabold'>Blogs</p>
+        </div>
+    </section>
+  )
+}
+
+export default Blog
